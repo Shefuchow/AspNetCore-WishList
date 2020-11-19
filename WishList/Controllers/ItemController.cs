ebe6902c8b068c20aedabb05291d9c303d5e9a67
shefuchow <mschowdh@buffelo.edu>
2020-11-18 21:32:48 -0500

    mod 5

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+public class ItemController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ItemController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var model = _context.Items.ToList();
+        return View("Index",model);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View("Create");
+    }
+
+    [HttpPost]
+    public IActionResult Create(WishList.Models.Item item)
+    {
+        _context.Items.Add(item);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        var item = _context.Items.FirstOrDefault(e => e.Id == id);
+        _context.Remove(item);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
