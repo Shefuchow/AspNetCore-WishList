1729f8a0db1d503d8dbb786f6637ef92b71cbd4d
shefuchow <mschowdh@buffelo.edu>
2020-11-18 21:35:45 -0500

    mod 5

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -26,7 +26,7 @@ namespace WishList.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(WishList.Models.Item item)
+        public IActionResult Create(Models.Item item)
         {
             _context.Items.Add(item);
             _context.SaveChanges();
